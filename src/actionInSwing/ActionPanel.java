7cd0a0edb2584a3c0bbe083e56b6f95809c562a1
LaunDan <launadaniel2@gmail.com>
2020-04-04 13:24:39 +0200

    add listener for button

--- src/actionInSwing/ActionPanel.java
+++ src/actionInSwing/ActionPanel.java
@@ -2,6 +2,8 @@ package actionInSwing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ActionPanel extends JPanel {
     private JButton button;
@@ -13,8 +15,18 @@ public class ActionPanel extends JPanel {
         button = new JButton("Button");
         this.add(button);
 
+        ListenerButton listener = new ListenerButton();
+        button.addActionListener(listener);
+
         text = new JLabel("Here is text");
         this.add(text);
+    }
+
+    private class ListenerButton implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            text.setText("You click on the button");
+        }
     }
 
 
