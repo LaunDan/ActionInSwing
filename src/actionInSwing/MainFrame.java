b432bb814cf0c7e1de8704b4f7084eaf3a617439
LaunDan <launadaniel2@gmail.com>
2020-04-05 17:51:46 +0200

    set MainFrame for timer

--- src/actionInSwing/MainFrame.java
+++ src/actionInSwing/MainFrame.java
@@ -5,13 +5,15 @@ import javax.swing.*;
 public class MainFrame extends JFrame {
 
     public MainFrame() {
-        this.setTitle("Action");
+        this.setTitle("Timer");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
 
         ActionPanel panel = new ActionPanel();
         this.add(panel);
         this.pack();
+        this.setResizable(false);
+
     }
 
     public static void main(String[] args) {
